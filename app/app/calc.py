b0b647451e27803d57e3d48e382ae9ac20b83cf9
Sethuraman Palaniappan <sethuramanpl@gmail.com>
2022-07-13 15:58:53 -0400

    postgres

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Calculator functions
+"""
+
+def add(x, y):
+    """ Add 2 numbers and return results"""
+    return x + y 
+
+def subtract(x, y):
+    """subtract x from y and return results"""
+    return y - x

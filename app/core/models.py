a4d920d4a43d9cdae6594fdb91079fbfef078451
Sethuraman Palaniappan <sethuramanpl@gmail.com>
2022-07-17 10:42:27 -0400

    admin page

--- app/core/models.py
+++ app/core/models.py
@@ -12,7 +12,7 @@ from django.contrib.auth.models import (
 class UserManager(BaseUserManager):
     """Manager for users."""
 
-    def create_user(self, email, password=None, **extra_fields):
+    def create_user(self, email, password, **extra_fields):
         """Create , save and return a new user."""
         if not email:
             raise ValueError('User must have an email address.')

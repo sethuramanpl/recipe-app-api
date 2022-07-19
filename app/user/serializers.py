0c03df06ac012fb25f41914243b0823156968577
Sethuraman Palaniappan <sethuramanpl@gmail.com>
2022-07-19 00:17:48 -0400

    authenticate

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -9,7 +9,6 @@ from django.contrib.auth import (
 
 from django.utils.translation import gettext as _
 
-
 from rest_framework import serializers
 
 
@@ -25,12 +24,23 @@ class UserSerializer(serializers.ModelSerializer):
         """Create and return a user with encrypted password."""
         return get_user_model().objects.create_user(**validated_data)
 
+    def update(self, instance, validated_data):
+        """Update and return user."""
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
+
 
 class AuthTokenSerializer(serializers.Serializer):
     """Serializer for the user auth token."""
-    email = serializer.EmailFiled()
-    password = serializer.CharField(
-        Style={'input_type': 'password'},
+    email = serializers.EmailField()
+    password = serializers.CharField(
+        style={'input_type': 'password'},
         trim_whitespace=False,
     )
 
